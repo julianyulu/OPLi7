4e02b6b7a1be748178f0af61d3b0e8a7e29ca2b4
Yu Lu <yulu@utexas.edu>
2017-09-28 18:56:43 -0500

    fixing errors, still have div not match error

--- main.py
+++ main.py
@@ -9,9 +9,9 @@
 # 
 # Created: Wed Sep 20 15:34:21 2017 (-0500)
 # Version: 
-# Last-Updated: Thu Sep 28 10:54:05 2017 (-0500)
-#           By: yulu
-#     Update #: 161
+# Last-Updated: Thu Sep 28 18:50:32 2017 (-0500)
+#           By: superlu
+#     Update #: 188
 # 
 
 
@@ -40,16 +40,17 @@ def main():
             for f in p.eStates:
                 popE[f] = [p.pop_Excited[f]]
         else:
-            G1, G2 = p.calGroundPop(popG1[i-1], popG2[i - 1] , popE0[i - 1], popE1[i - 1], popE2[i - 1], popE3[i - 1], I1, I2, dt)
-            newEPop = p.calExcitedPop(popG, popE, I1, I2, dt)
-        for f in p.eStates:
-            popE[f].append(newEPop[f])
-        popG['F1'].append(G1)
-        popG['F2'].append(G2)
+            newPopG = p.calGroundPop(popG, popE, i-1, I1, I2, dt)
+            newPopE = p.calExcitedPop(popG, popE, i-1, I1, I2, dt)
+            for f in p.eStates:
+                popE[f].append(newPopE[f])
+            popG['F1'].append(newPopG['F1'])
+            popG['F2'].append(newPopG['F2'])
 
-        if abs(p.checkUniformity(G1,G2,E0, E1, E2, E3) - 1) > 0.1:
-            print("Total population: ", p.checkUniformity(G1,G2,E0, E1, E2, E3), " off too much")
-            return 0 
+            unitCheck = p.checkUniformity(newPopG, newPopE)
+            if abs( unitCheck- 1) > 0.1:
+                print("Total population: ", unitCheck, " off too much")
+                return 0 
     clock = np.linspace(0, totalTime, numSteps) * 1e9
 
     
@@ -86,41 +87,11 @@ def main():
     ax2 = fig2.add_subplot(111)
     #cm2 = plt.get_cmap('magma', linewidth = lw)
     #ax2.set_prop_cycle(cycler('color', [cm2(1. * i / 16) for i in range(16)]))
-    ax2.plot(clock, [x[0][0] for x in popE0], "-", label = "F = 0, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE1], "-", label = "F = 1, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE1], "-", label = "F = 1, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE1], "-", label = "F = 1, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE2], "-", label = "F = 2, m = -2", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE2], "-", label = "F = 2, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE2], "-", label = "F = 2, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][3] for x in popE2], "-", label = "F = 2, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][4] for x in popE2], "-", label = "F = 2, m = 2", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE3], "-", label = "F = 3, m = -3", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE3], "-", label = "F = 3, m = -2", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE3], "-", label = "F = 3, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][3] for x in popE3], "-", label = "F = 3, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][4] for x in popE3], "-", label = "F = 3, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][5] for x in popE3], "-", label = "F = 3, m = 2", linewidth = lw)
-    ax2.plot(clock, [x[0][6] for x in popE3], "-", label = "F = 3, m = 3", linewidth = lw)
+    for f in p.eStates:
+        fNum = int(f[-1])
+        for i in range(2 * fNum + 1):
+            ax2.plot(clock, [x[0][i] for x in popE[f]], "-", label = "F=" + str(fNum) + ", m=" + -fNum+ i, linewidth = lw)
 
-    '''
-    ax2.plot(clock, [x[0][0] for x in popE0], "-", label = "F = 0, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE1], ":", label = "F = 1, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE1], ":", label = "F = 1, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE1], ":", label = "F = 1, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE2], "-.", label = "F = 2, m = -2", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE2], "-.", label = "F = 2, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE2], "-.", label = "F = 2, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][3] for x in popE2], "-.", label = "F = 2, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][4] for x in popE2], "-.", label = "F = 2, m = 2", linewidth = lw)
-    ax2.plot(clock, [x[0][0] for x in popE3], "--", label = "F = 3, m = -3", linewidth = lw)
-    ax2.plot(clock, [x[0][1] for x in popE3], "--", label = "F = 3, m = -2", linewidth = lw)
-    ax2.plot(clock, [x[0][2] for x in popE3], "--", label = "F = 3, m = -1", linewidth = lw)
-    ax2.plot(clock, [x[0][3] for x in popE3], "--", label = "F = 3, m = 0", linewidth = lw)
-    ax2.plot(clock, [x[0][4] for x in popE3], "--", label = "F = 3, m = 1", linewidth = lw)
-    ax2.plot(clock, [x[0][5] for x in popE3], "--", label = "F = 3, m = 2", linewidth = lw)
-    ax2.plot(clock, [x[0][6] for x in popE3], "--", label = "F = 3, m = 3", linewidth = lw)
-    '''
     ax2.legend()
     
     #fig.savefig("./test.png")
--- optPumping.py
+++ optPumping.py
@@ -9,9 +9,9 @@
 # 
 # Created: Sun Sep 17 16:36:41 2017 (-0500)
 # Version: 
-# Last-Updated: Wed Sep 27 23:50:28 2017 (-0500)
-#           By: yulu
-#     Update #: 231
+# Last-Updated: Thu Sep 28 18:50:48 2017 (-0500)
+#           By: superlu
+#     Update #: 256
 # 
 
 
@@ -23,14 +23,19 @@ class optPumping:
     
     def __init__(self, Dline,  pumpPol1, pumpPol2):
         
-        if self.Dline == 'D1':
+        if Dline == 'D1':
             from TransitionStrength import TransStrengthD1 as TransStrength
             from TransitionStrength import DecayStrengthD1 as DecayStrength
-        elif self.Dline == 'D2':
+        elif Dline == 'D2':
             from TransitionStrength import TransStrengthD2 as TransStrength
             from TransitionStrength import DecayStrengthD2 as DecayStrength
         else:
             print('Unavaliable D line transition !')
+        # I don't think this is necessary
+        #self.TransStrength = TransStrength
+        #self.DecayStrength = DecayStrength
+
+        
         # Initialize pump matrix 
         try:
             self.pumpMatrix1 = eval('TransStrength.' + pumpPol1) # select pumping matrix based on polarization
@@ -64,14 +69,14 @@ class optPumping:
         
         # Initialize ground level population
         self.pop_Ground ={
-            'F1': np.ones([1,3]) * 3./8
+            'F1': np.ones([1,3]) * 3./8,
             'F2': np.ones([1,5]) * 5./8
             }
 
         # Initialize excited level population
         self.pop_Excited = {}
-        for s,n in zip(self.DecayStrength.eStates, self.DecayStrength.numSubStates):
-            self.pop_Excited{s:np.zeros([1, n])}
+        for s,n in zip(DecayStrength.eStates, DecayStrength.numSubStates):
+            self.pop_Excited[s] = np.zeros([1, n])
         
         # Calculate overal factor for dipole matrix
         self.dipoleFactor = self.dipoleScaleFactor()
@@ -94,42 +99,45 @@ class optPumping:
         return mtx.sum(axis = 1)
 
     
-    def calGroundPop(self, popGround, popExcited, I1, I2, dt):
-        G1 = popGround['F1']
-        G2 = popGround['F2']
+    def calGroundPop(self, popGround, popExcited, idx, I1, I2, dt):
+        G1 = popGround['F1'][idx]
+        G2 = popGround['F2'][idx]
         newG1 = np.zeros([1, len(G1[0])])
         newG2 = np.zeros([1, len(G2[0])])
+        
         for es in self.eStates:
+            print(popExcited[es][idx].shape)
             newG1 += -self.vectorizeMatrix(eval("self.pumpMatrix1.F1_D2_" + es)).T * G1 * I1\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.sigmaPlus." + es + "_" + self.Dline + "_F1"))\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.sigmaMinus." + es + "_" + self.Dline + "_F1"))\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.pi." + es + "_" + self.Dline + "_F1"))
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.sigmaPlus." + es + "_" + self.Dline + "_F1"))\
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.sigmaMinus." + es + "_" + self.Dline + "_F1"))\
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.pi." + es + "_" + self.Dline + "_F1"))
             newG2 += -self.vectorizeMatrix(eval("self.pumpMatrix1.F1_D2_" + es)).T * G2 * I2\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.sigmaPlus." + es + "_" + self.Dline + "_F2"))\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.sigmaMinus." + es + "_" + self.Dline + "_F2"))\
-                     + np.dot(popExcited[es], eval("self.decayMatrix.pi." + es + "_" + self.Dline + "_F2"))
-            
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.sigmaPlus." + es + "_" + self.Dline + "_F2"))\
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.sigmaMinus." + es + "_" + self.Dline + "_F2"))\
+                     + np.dot(popExcited[es][idx], eval("self.decayMatrix.pi." + es + "_" + self.Dline + "_F2"))
         newG1 = G1 + newG1 * self.dipoleFactor * dt  
         newG2 = G2 + newG2 * self.dipoleFactor * dt
-        return (newG1, newG2)
+        pop['F1'] = newG1
+        pop['F2'] = newG2
+        return pop
 
-    def calExcitedPop(self, popGround, popExcited, I1, I2, dt):
+    def calExcitedPop(self, popGround, popExcited, idx, I1, I2, dt):
         newE = {}
         for es in self.eStates: # loop thru excited states names
-            newE[es] = np.zeros([1, len(popExcited[es])])
+            newE[es] = np.zeros([1, len(popExcited[es][idx][0])])
         for p in self.pol:
             for gs,I, pumpMatrix in zip(['F1', 'F2'], [I1, I2], [self.pumpMatrix1, self.pumpMatrix2]):
                 for es in self.eStates: # loop thru excited hyperfine states names 
                 # 3.0 factor is to compensate repeating sum of polarization
-                newE[es] += np.dot(popGround[gs], eval("pumpMatrix." + gs + "_" + self.Dline + "_" + es)) / 3.0 * I\
-                            - self.vectorizeMatrix(eval("self.decayMatrix." + p + "." + es + "_" + self.Dline + "_" + gs)).T * popExcited[es]
+                    newE[es] += np.dot(popGround[gs][idx], eval("pumpMatrix." + gs + "_" + self.Dline + "_" + es)) / 3.0 * I\
+                            - self.vectorizeMatrix(eval("self.decayMatrix." + p + "." + es + "_" + self.Dline + "_" + gs)).T * popExcited[es][idx]
         for es in self.eStates:
-            newE[es] = popExcited[es] + newE[es] * self.dipoleFactor * dt
+            newE[es] = popExcited[es][idx] + newE[es][idx] * self.dipoleFactor * dt
         return newE
                 
     
     
-    def checkUniformity(self, popGround, popExcited):
+    def checkUniformity(self, popGround,  popExcited):
         
-        return popGround['F1'].sum() + popGround['F2'] + sum[popExcited[str(x)].sum() for x in popExcited]
+        return popGround['F1'].sum() + G2.sum() + sum([popExcited[str(x)].sum() for x in popExcited])
     
